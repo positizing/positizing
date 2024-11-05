ce35d7dd9973b2fdfcf53df969e5ca05ba914738
James X. Nelson <James@WeTheInter.net>
2024-11-04 22:18:54 -0600

    Add ChatGPT conjunction and profanity detector
    Thanks chatgpt

--- detector/src/main/java/injuction/detector/InjunctionDetector.java
+++ detector/src/main/java/injuction/detector/InjunctionDetector.java
@@ -1,18 +1,15 @@
 package injuction.detector;
 
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations;
-import edu.stanford.nlp.util.CoreMap;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -26,84 +23,112 @@ public class InjunctionDetector {
     // list of PartsOfSpeech types here: https://surdeanu.cs.arizona.edu//mihai/teaching/ista555-fall13/readings/PennTreebankConstituents.html
     private static final String TYPE_ADVERB = "RB";
     private static final String TYPE_MODAL = "MD";
+    private static final String TYPE_CONJUNCTION = "CC";
+
     private static final Set<String> ADVERB_MATCH_LIST = new HashSet<>();
     private static final Set<String> MODAL_MATCH_LIST = new HashSet<>();
+    private static final Set<String> CONJUNCTION_MATCH_LIST = new HashSet<>();
+    private static final Set<String> PROFANITY_MATCH_LIST = new HashSet<>();
+
     static {
         ADVERB_MATCH_LIST.add("not");
         ADVERB_MATCH_LIST.add("never");
         ADVERB_MATCH_LIST.add("n't");
         ADVERB_MATCH_LIST.add("n’t");
         MODAL_MATCH_LIST.add("should");
+
+        // Initialize conjunction match list
+        CONJUNCTION_MATCH_LIST.add("but");
+
+        // Load profane words from 'profane_words.txt'
+        try (InputStream inputStream = InjunctionDetector.class.getClassLoader().getResourceAsStream("profanity/profane_words.txt")) {
+            if (inputStream != null) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        PROFANITY_MATCH_LIST.add(line.trim().toLowerCase(Locale.ROOT));
+                    }
+                }
+            } else {
+                System.err.println("profane_words.txt not found in resources.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     private final StanfordCoreNLP pipeline;
 
     public InjunctionDetector() {
         Properties props = new Properties();
-//        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-        props.put("annotators", "tokenize, pos");
+        props.put("annotators", "tokenize, pos, lemma");
         pipeline = new StanfordCoreNLP(props);
     }
 
-    public static void main(String ... args) {
+    public static void main(String... args) {
+        InjunctionDetector detector = new InjunctionDetector();
 
+        List<String> testMessages = Arrays.asList(
+                "I will not finish in time.",
+                "I wanted to go, but it was too late.",
+                "This is a clean sentence.",
+                "I can't do this.",
+                "She tried her best, but failed.",
+                "Nothing can stop us now."
+        );
+
+        for (String message : testMessages) {
+            boolean hasInjunction = detector.isInjuction(message);
+            boolean hasProfanity = detector.containsProfanity(message);
+
+            System.out.println("Message: " + message);
+            System.out.println("Contains Injunction: " + hasInjunction);
+            System.out.println("Contains Profanity: " + hasProfanity);
+            System.out.println("-----------------------------------");
+        }
     }
 
     public boolean isInjuction(final String message) {
-
-        // create an empty Annotation just with the given text
         Annotation document = new Annotation(message);
-
-        // run all Annotators on this text
         pipeline.annotate(document);
+        List<CoreLabel> tokens = document.get(CoreAnnotations.TokensAnnotation.class);
 
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreLabel token : tokens) {
+            String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            String word = token.originalText().toLowerCase(Locale.ROOT);
 
-        for(CoreMap sentence: sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
-            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                // this is the text of the token
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                // this is the POS tag of the token
-                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-                switch (pos) {
-                    case TYPE_ADVERB:
-                        if (ADVERB_MATCH_LIST.contains(token.originalText().toLowerCase(Locale.ROOT))) {
-                            return true;
-                        }
-                        break;
-                    case TYPE_MODAL:
-                        if (MODAL_MATCH_LIST.contains(token.originalText().toLowerCase(Locale.ROOT))) {
-                            return true;
-                        }
-                        break;
-
-                }
-
-                // this is the NER label of the token
-                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-
-                System.out.println("word: " + word + " pos: " + pos + " ne:" + ne);
+            switch (pos) {
+                case TYPE_ADVERB:
+                    if (ADVERB_MATCH_LIST.contains(word)) {
+                        return true;
+                    }
+                    break;
+                case TYPE_MODAL:
+                    if (MODAL_MATCH_LIST.contains(word)) {
+                        return true;
+                    }
+                    break;
+                case TYPE_CONJUNCTION:
+                    if (CONJUNCTION_MATCH_LIST.contains(word)) {
+                        return true;
+                    }
+                    break;
             }
-
-            // this is the parse tree of the current sentence
-            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-            System.out.println("parse tree:\n" + tree);
-
-            // this is the Stanford dependency graph of the current sentence
-//            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
-//            System.out.println("dependency graph:\n" + dependencies);
         }
+        return false;
+    }
 
-        // This is the coreference link graph
-        // Each chain stores a set of mentions that link to each other,
-        // along with a method for getting the most representative mention
-        // Both sentence and token offsets start at 1!
-        Map<Integer, CorefChain> graph =
-                document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
+    public boolean containsProfanity(final String message) {
+        Annotation document = new Annotation(message);
+        pipeline.annotate(document);
+        List<CoreLabel> tokens = document.get(CoreAnnotations.TokensAnnotation.class);
 
+        for (CoreLabel token : tokens) {
+            String lemma = token.get(CoreAnnotations.LemmaAnnotation.class).toLowerCase(Locale.ROOT);
+            if (PROFANITY_MATCH_LIST.contains(lemma)) {
+                return true;
+            }
+        }
         return false;
     }
 }
